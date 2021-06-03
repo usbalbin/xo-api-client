b10208ed9b4848b158669b8bbb950c54d8b6063f
Albin Hedman <albin9604@gmail.com>
2021-06-04 00:49:31 +0200

    Update example

--- examples/list_vms.rs
+++ examples/list_vms.rs
@@ -1,27 +1,5 @@
-use std::{collections::BTreeMap, env};
-
-use async_trait::async_trait;
-use xo_api_client::{Client, Vm, VmId};
-
-const TOKEN_FILE: &str = "my_secret_token";
-
-#[derive(Debug)]
-struct TokenHandler;
-
-#[async_trait]
-impl xo_api_client::TokenHandler for TokenHandler {
-    type SaveErr = std::io::Error;
-
-    type LoadErr = std::io::Error;
-
-    async fn save(&mut self, token: &str) -> Result<(), Self::SaveErr> {
-        Ok(std::fs::write(TOKEN_FILE, token)?)
-    }
-
-    async fn load(&self) -> Result<String, Self::LoadErr> {
-        std::fs::read_to_string(TOKEN_FILE)
-    }
-}
+use std::collections::BTreeMap;
+use xo_api_client::{credentials::EmailAndPassword, Client, Vm, VmId};
 
 // We dont care about any of the data under the "other" attribute
 // in this example
@@ -33,26 +11,15 @@ impl xo_api_client::vm::OtherInfo for OtherInfo {}
 #[tokio::main]
 async fn main() {
     let url = "ws://localhost:8080/api/";
+    let email = String::from("admin@admin.net");
+    let password = String::from("admin");
 
-    match env::args().nth(1).as_deref() {
-        Some("login") => {
-            let username = String::from("admin@admin.net");
-            let password = String::from("admin");
-            Client::sign_in(url, username, password, &mut TokenHandler)
-                .await
-                .expect("Failed to sign in");
-            return;
-        }
-        Some(_) => {
-            println!("Usage: list_vms [login]");
-            return;
-        }
-        None => (),
-    }
-
-    let con = Client::connect(url, &mut TokenHandler)
+    let con = Client::connect(url)
         .await
-        .expect("Failed to connect to server, have you signed in?");
+        .expect("Failed to connect to server");
+    con.sign_in(EmailAndPassword { email, password })
+        .await
+        .expect("Failed to sign in");
 
     let all_vms: BTreeMap<VmId, Vm<OtherInfo>> =
         con.get_vms(None, None).await.expect("Failed to list VMs");
